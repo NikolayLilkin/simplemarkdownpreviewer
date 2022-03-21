3388b3ccc5cd817bbd8c0482227ff92471a4a99a
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-03-21 14:03:27 +0200

    push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Firstcontainer from './components/Firstcontainer';
-import Secondcontainer from './components/Secondcontainer';
 
 function App() {
   return (
     <div className="App">
       <Firstcontainer/>
-      <Secondcontainer/>
     </div>
   );
 }
