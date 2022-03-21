fa72dcc89e0ecaa8b55ba9b6bc0a9027a064965c
Nikolay Anatoliev Lilkin <nikolayanatolievlilkin@gmail.com>
2022-03-21 13:05:12 +0200

    Created FirstContainer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import './components/firstContainer.js'
 function App() {
   return (
     <div className="App">
-      
+      <firstContainer/>
     </div>
   );
 }
---
+++ src/components/firstContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+class firstContainer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    render() {
+        return(
+           <div>
+               <h1>Hello my name is Nikolay</h1>
+           </div> 
+        );
+    }
+}
+export default firstContainer; 
